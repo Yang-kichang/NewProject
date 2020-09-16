d34c05819f6b510ddea87e75f5f536b036d15d93
Yang-Kichang <ykcha9@naver.com>
2020-09-15 19:46:31 -0700

    modified c

--- hello.c
+++ hello.c
@@ -1,6 +1,7 @@
   #include <stdio.h>
    
   int main() {
-       printf("%s\n", "오픈소스기반기초설계");
-       return 0;
+    printf("%s\n", "오픈소스기반기초설계");
+	printf("%s\n", "오픈소스기반기초설계");
+    return 0;
   }
